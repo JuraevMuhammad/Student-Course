50116f29d568609023380a46883aa88ddec5c0e4
juraev <muhammadjonjuraev18@gmail.com>
2025-12-26 17:55:22 +0500

    new commit

--- Domain/Student.cs
+++ Domain/Student.cs
@@ -7,4 +7,36 @@ public class Student
     public string LastName { get; set; }
     public int BirthYear { get; set; }
     public string Address { get; set; }
+
+    private int cnt = 0;
+    public Student(string firstName, string lastName, int birthYear, string address)
+    {
+        cnt++;
+        Id = cnt;
+        FirstName = firstName;
+        LastName = lastName;
+        BirthYear = birthYear;
+        Address = address;
+    }
+    
+    public Student()
+    {
+        cnt++;
+        Id = cnt;
+    }
+
+    public Student(int id, string firstName, string lastName, int birthYear, string address)
+    {
+        cnt++;
+        Id = cnt;
+        FirstName = firstName;
+        LastName = lastName;
+        BirthYear = birthYear;
+        Address = address;
+    }
+
+    public void GetInfo()
+    {
+        Console.WriteLine($"Id: {Id}, FirstName: {FirstName}, LastName: {LastName}");
+    }
 }
--- Service/StudentService.cs
+++ Service/StudentService.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.IO.Enumeration;
 using Domain;
 using Domain.Interface;
 
@@ -12,73 +13,217 @@ public class StudentService : IStudent, ICourse
     // Student Methods
     public void AddStudent(Student student)
     {
-        throw new NotImplementedException();
+        _students.Add(student);
+        Console.WriteLine($"Student {student.Id} has been added");
     }
 
     public void GetAllStudents()
     {
-        throw new NotImplementedException();
+        foreach (var student in _students)
+        {
+            student.GetInfo();
+        }
+        Console.WriteLine("===================");
     }
 
     public void UpdateStudent(Student student)
     {
-        throw new NotImplementedException();
+        var result = _students.Find(x => x.Id == student.Id);
+        if (result != null)
+        {
+            result.FirstName = student.FirstName;
+            result.LastName = student.LastName;
+            result.BirthYear = student.BirthYear;
+            result.Address = student.Address;
+            Console.WriteLine($"Student {result.Id} has been updated");
+            return;
+        }
+
+        Console.WriteLine($"Student {student.Id} not found");
     }
 
     public void DeleteStudent(int id)
     {
-        throw new NotImplementedException();
+        var result = _students.Find(x => x.Id == id);
+        if (result != null)
+        {
+            _students.Remove(result);
+            Console.WriteLine($"Student {result.Id} has been deleted");
+            return;
+        }
+        Console.WriteLine($"Student {id} has been deleted");
     }
 
     public List<Student> SearchStudentByName(string name)
     {
-        throw new NotImplementedException();
+        var result = _students.Where(x => x.FirstName.ToLower().Contains(name.ToLower())).ToList();
+        if (result != null)
+        {
+            Console.WriteLine("---Students found---");
+            return result;
+        }
+
+        return null;
     }
 
+    #region GetStudentsWithoutCourses
+    private List<Student> resStudents = new();
     public List<Student> GetStudentsWithoutCourses()
     {
-        throw new NotImplementedException();
-    }
+        try
+        {
+            foreach (var student in _students)
+            {
+                foreach (var course in _courses)
+                {
+                    if (student.Id != course.Id)
+                    {
+                        resStudents.Add(student);
+                    }
+                }
+            }
 
+            if (resStudents.Count == 0)
+            {
+                throw new Exception("No students found");
+            }
+
+            return resStudents;
+        }
+        catch
+        {
+            throw new Exception("Error getting students");
+        }
+    }
+    #endregion
+
+    #region GetStudentsWithMoreThanNCourses
+
+    private List<Student> _getStudent = new();
+    private List<Student> _getStudents = new();
     public List<Student> GetStudentsWithMoreThanNCourses(int n)
     {
-        throw new NotImplementedException();
-    }
+        try
+        {
+            foreach (var student in _students)
+            {
+                foreach (var course in _courses)
+                {
+                    if (student.Id == course.Id)
+                    {
+                        _getStudent.Add(student);
+                    }
 
+                    if (_getStudent.Count > n)
+                    {
+                        _getStudents.Add(student);
+                    }
+                }
+            }
+
+            if (_getStudents.Count == 0)
+            {
+                return null;
+            }
+
+            return _getStudent;
+        }
+        catch
+        {
+            throw new Exception("Error getting students");
+        }
+    }
+    #endregion
+
+    #region GetStudentsByAddress
+
+    private List<Student> _getStudentsByAddress = new();
     public List<Student> GetStudentsByAddress(string text)
     {
-        throw new NotImplementedException();
+        var res = _getStudentsByAddress.Where(x => x.Address.ToLower().Contains(text.ToLower())).ToList();
+        if (res.Count == 0)
+        {
+            return null;
+        }
+        else
+        {
+            return res;
+        }
     }
 
+    #endregion
+
+    #region GetStudentsByBirthYear
+
+    private List<Student> _getStudentsByBirthYear = new();
     public List<Student> GetStudentsByBirthYear(int year)
     {
-        throw new NotImplementedException();
+        var res = _students.Where(x => x.BirthYear == year).ToList();
+        if (res.Count == 0)
+        {
+            Console.WriteLine("No students found");
+            return null;
+        }
+        return res;
     }
 
+    #endregion
+
+    #region SortStudentsByLastName
+
+    private List<Student> _sortStudent = new();
     public List<Student> SortStudentsByLastName()
     {
-        throw new NotImplementedException();
+        _sortStudent = _students.OrderBy(x => x.LastName).ToList();
+        return _sortStudent;
     }
+
+    #endregion
+
+    #region GetYoungestStudent
 
     public Student GetYoungestStudent()
     {
-        throw new NotImplementedException();
+        var res = _students.Max(x => x.BirthYear);
+        var result = _students.FirstOrDefault(x => x.BirthYear == res);
+
+        return result;
     }
+
+    #endregion
+
+    #region GetOldestStudent
 
     public Student GetOldestStudent()
     {
-        throw new NotImplementedException();
+        var res = _students.OrderBy(x => x.BirthYear).FirstOrDefault();
+        return res;
     }
+
+    #endregion
+
+    #region ExistsStudent
 
     public bool ExistsStudent(int id)
     {
-        throw new NotImplementedException();
+        var res = _students.Where(x => x.Id == id).Take(1).FirstOrDefault();
+        if (res == null)
+        {
+            return false;
+        }
+        return true;
     }
+
+    #endregion
+
+    #region DeleteAllCoursesOfStudent
 
     public void DeleteAllCoursesOfStudent(int studentId)
     {
-        throw new NotImplementedException();
+        _courses.RemoveAll(x => x.Id == studentId);
     }
+
+    #endregion
 
     public void RemoveStudentsWithoutAddress()
     {
